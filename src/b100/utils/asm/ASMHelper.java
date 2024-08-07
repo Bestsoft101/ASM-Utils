2b2a79e56793b0b60a265aa65102bf43e4453c98
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2024-08-07 23:36:15 +0200

    findAllMethods method

--- src/b100/utils/asm/ASMHelper.java
+++ src/b100/utils/asm/ASMHelper.java
@@ -221,15 +221,8 @@ public abstract class ASMHelper {
 		
 		return foundMethods.get(0);
 	}
-	
 	public static MethodNode findMethod(ClassNode classNode, Condition<MethodNode> condition) {
-		List<MethodNode> foundMethods = new ArrayList<MethodNode>();
-		
-		for(MethodNode methodNode : classNode.methods) {
-			if(condition.isTrue(methodNode)) {
-				foundMethods.add(methodNode);
-			}
-		}
+		List<MethodNode> foundMethods = findAllMethods(classNode, condition);
 		
 		if(foundMethods.size() != 1) {
 			StringBuilder msg = new StringBuilder();
@@ -251,6 +244,16 @@ public abstract class ASMHelper {
 		}
 		
 		return foundMethods.get(0);
+	}
+	
+	public static List<MethodNode> findAllMethods(ClassNode classNode, Condition<MethodNode> condition) {
+		List<MethodNode> foundMethods = new ArrayList<MethodNode>();
+		for(MethodNode methodNode : classNode.methods) {
+			if(condition.isTrue(methodNode)) {
+				foundMethods.add(methodNode);
+			}
+		}
+		return foundMethods;
 	}
 
 	public static List<AbstractInsnNode> findAllInstructions(MethodNode method, Condition<AbstractInsnNode> condition) {
